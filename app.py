9a2da01fb67108c358ac7bea836b8eedc0c99559
NakalanziJacklyn <nakalanzijacklyn98@gmail.com>
2019-01-29 20:14:36 +0300

    my hosted trial

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+trial = Flask(__name__)
+
+@trial.route('/')
+def hello_world():
+    return render_template('home.html')
+
+    @trial.route('/')
+def hello_world():
+    return 'This is my about page'
